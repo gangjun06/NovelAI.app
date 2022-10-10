72c82ad98c37efc65b6a34d7077c1e88f90fa1b8
이강준 <me@gangjun.dev>
2022-10-10 20:19:42 +0900

    feat: apply google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,25 @@ import type { AppProps } from "next/app";
 import { DragDropContext, resetServerContext } from "react-beautiful-dnd";
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      //@ts-ignore
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Toaster />
---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
