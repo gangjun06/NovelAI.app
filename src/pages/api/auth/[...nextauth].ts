0149b295c446f522bcf92624b9bd12978f82fdae
이강준 <me@gangjun.dev>
2022-10-29 11:54:19 +0000

    feat: add prisma db and next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import DiscordProvider from 'next-auth/providers/discord'
+import KakaoProvider from 'next-auth/providers/kakao'
+import NaverProvider from 'next-auth/providers/naver'
+import TwitterProvider from 'next-auth/providers/twitter'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
+import prisma from '~/lib/prisma'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      version: '2.0', // opt-in to Twitter OAuth 2.0
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
---
+++ src/types/env.d.ts
@@ -0,0 +1,16 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string
+    NEXTAUTH_URL: string
+    NEXT_PUBLIC_GA_ID: string
+    NODE_ENV: 'development' | 'production'
+    DISCORD_CLIENT_ID: string
+    DISCORD_CLIENT_SECRET: string
+    KAKAO_CLIENT_ID: stirng
+    KAKAO_CLIENT_SECRET: stirng
+    NAVER_CLIENT_ID: string
+    NAVER_CLIENT_SECRET: string
+    TWITTER_ID: string
+    TWITTER_SECRET: string
+  }
+}
