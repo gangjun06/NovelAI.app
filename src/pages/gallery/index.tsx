89c1f599464d844d206ab2cd23395d518d224134
이강준 <me@gangjun.dev>
2022-11-04 03:16:49 +0000

    feat: design gallery main page

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,13 +1,16 @@
 import { useCallback, useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Image from 'next/image'
+import { Image as ImageType } from '@prisma/client'
+import axios from 'axios'
 
+import { galleryURL } from '~/assets/urls'
+import { Button, Input } from '~/components/atoms'
 import { MainTemplate } from '~/components/template'
 
 import Masonry from 'react-masonry-css'
-import axios from 'axios'
-import { galleryURL } from '~/assets/urls'
-import { Image as ImageType } from '@prisma/client'
+import { CloudArrowUpIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const Loader = () => <div>데이터를 불러오고 있어요</div>
 
@@ -43,7 +46,13 @@ const GalleryPage = () => {
 
   return (
     <MainTemplate title="갤러리" description="인공지능으로 생성된 이미지들을 공유하세요">
-      <div></div>
+      <div className="max-w-4xl mx-auto my-8 px-6">
+        <div className="text-4xl text-title-color font-bold">AI 이미지 갤러리</div>
+        <div className="mt-2 flex gap-x-2">
+          <Input className="w-full" placeholder="검색어를 입력해주세요..." />
+          <Button variant="primary">검색</Button>
+        </div>
+      </div>
       <InfiniteScroll
         className="h-full w-11/12 mx-auto"
         next={fetchNext}
@@ -56,6 +65,12 @@ const GalleryPage = () => {
           columnClassName="my-masonry-grid_column flex flex-col gap-y-3"
           breakpointCols={breakpointColumns}
         >
+          <Link href="/gallery/new" passHref>
+            <a className="h-48 p-5 boarder border-2 border-base-color border-dashed text-subtitle-color flex items-center justify-center flex-col hover:brightness-70">
+              <div>내 이미지 업로드하기</div>
+              <CloudArrowUpIcon className="w-8 h-8" />
+            </a>
+          </Link>
           {images.map((item) => (
             <div key={item.id}>
               <Image
